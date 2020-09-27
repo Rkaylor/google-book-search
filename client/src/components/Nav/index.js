81a4b4089ee756ba0429dd1ac07f51bccec42f2f
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-27 00:36:58 -0400

    Fixed the saved.js page to be able to properly export the proper structure for Saved

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,33 +4,33 @@ import "./style.css";
 
 //  YOUR NAV COMPONENT HERE
 function Nav(props) {
-  return(
-  <nav className="navbar navbar-expand-sm navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-          Google Books
-      </Link>
-      <div>
-      <ul className="navbar-nav margin-left">
-          <li className="nav-item">
-          <Link to="/search"
-              className={
-              window.location.pathname === "/" ||
-              window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-          >
-              Search
-          </Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/saved"
-              className={
-              window.location.pathname === "/saved"  ? "nav-link active"  : "nav-link"}
-          >
-              Saved
-          </Link>
-          </li>
-      </ul>
-      </div>
-  </nav>
-  );
+        return(
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+            <Link className="navbar-brand" to="/">
+                Google Books
+            </Link>
+            <div>
+            <ul className="navbar-nav margin-left">
+                <li className="nav-item">
+                <Link to="/search"
+                    className={
+                    window.location.pathname === "/" ||
+                    window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+                >
+                    Search
+                </Link>
+                </li>
+                <li className="nav-item">
+                <Link to="/saved"
+                    className={
+                    window.location.pathname === "/saved"  ? "nav-link active"  : "nav-link"}
+                >
+                    Saved
+                </Link>
+                </li>
+            </ul>
+            </div>
+        </nav>
+        );
 }
 export default Nav;
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Jumbotron from "../components/Jumbotron";
 import Card from "../components/Card";
 import Book from "../components/Book";
@@ -7,35 +7,80 @@ import API from "../utils/API";
 import { Col, Row, Container } from "../components/Grid";
 import { List } from "../components/List";
 
-class Saved extends Component {
-  state = {
+function Saved() {
+  const [state, setState] = useState({
     books: []
-  };
+  });
 
-  componentDidMount() {
-    this.getSavedBooks();
-  }
+  useEffect(() => {
+    getSavedBooks();
+  },[])
 
-  getSavedBooks = () => {
+  const getSavedBooks = () => {
     API.getSavedBooks()
-      .then(res =>
-        this.setState({
+      .then(res => {
+        setState({
+          ...state,
           books: res.data
-        })
-      )
+        });
+        console.log(res.data);
+      })
       .catch(err => console.log(err));
   };
 
-  handleBookDelete = id => {
-    API.deleteBook(id).then(res => this.getSavedBooks());
+  const handleBookDelete = id => {
+    API.deleteBook(id).then(res => getSavedBooks());
   };
 
-  render () {
+
     return (
-      // YOUR CODE HERE
-<div></div>
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1 className="text-center">
+                <strong>(React) Google Books Search</strong>
+              </h1>
+              <h2 className="text-center">Search for and Save Books of Interest.</h2>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <Card title="Results" icon="fa fa-bookmark">
+              {state.books.length ? (
+                <List>
+                  {state.books.map(book => (
+                    <Book
+                      key={book._id}
+                      title={book.title}
+                      subtitle={book.subtitle}
+                      link={book.link}
+                      // authors={book.volumeInfo.authors.join(", ")}
+                      authors={book.authors}
+                      description={book.description}
+                      image={book.image}
+                      Button={() => (
+                        <button
+                          onClick={() => handleBookDelete(book._id)}
+                          className="btn btn-primary ml-2"
+                        >
+                          Delete
+                        </button>
+                      )}
+                    />
+                  ))}
+                </List>
+              ) : (
+                <h2 className="text-center">There is no saved books! Search Books!</h2>
+              )}
+            </Card>
+          </Col>
+        </Row>
+        <Footer />
+      </Container>
     );
-  }
+  
 }
 
 export default Saved;
