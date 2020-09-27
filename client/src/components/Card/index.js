61ede668c5028ba1a6543d46a63617d37fcd2762
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-27 00:19:03 -0400

    Added the proper components to card to display.

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-function Card(/* YOUR CODE HERE */) {
+function Card(props) {
   return (
     // YOUR CODE HERE
-    <div></div>
+    function Card(props) {
+      return (
+        <div className="card text-center">
+          <div className="card-header">
+            <h4>
+              <i className={props.icon}></i>
+              {props.title}
+            </h4>
+          </div>
+          <div className="card-body">{props.children}</div>
+        </div>
+      );
+    }
   );
 }
 
